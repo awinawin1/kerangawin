bc5e2a18b8630b8d74576bc98afb766c3452f335
awinawin1 <awinawin0014@gmail.com>
2018-09-23 14:25:05 +0700

    new1

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def handle_message(event):
     if text=="where?":
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='RPL'))
     if text=="why?":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='pingin aja.'))
+        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id='1', sticker_id='1'))
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
 
