f9b5902ace52a841929a2a52e408eefb036cb0b8
awinawin1 <43516997+awinawin1@users.noreply.github.com>
2018-10-20 07:27:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ import sys, random
 import tempfile
 import requests
 import re
-import requests, json
 
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
@@ -36,25 +35,26 @@ from linebot.models import (
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('fCHDuc8hHkLz0tvOznXtKuRnYbPU/ECyD9jrukCXD6wezR1m8aTPJBkdyqQE9kzpX+2Qn+AMdgdheLyIVSKDn/T14oxg0IDgs8IXD60kEMjVQF1JRpT7V5/2hqUu6AeUZ4llz7zmrUeGo7wWohnxzQdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('iN84VXXxDZEG6iZHl4TmHACqEBPsYfz0c7Vd8Lbzv7e64kvXRpq5Jnh9NTqULp9G/B3ucawJAu6nf9VKP+9INrOfUD6cEaAIpY9pCC/QlyiAuHzPoueALcT0zM40hQte/bFjcwSKEzCGFdKlfsAJUwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('3b1e2ee8b8223f9b4eae77269ffc02a0')
+handler = WebhookHandler('4a04bf97f085104ce2a7044775665ce2')
 #===========[ NOTE SAVER ]=======================
 notes = {}
 
-#input mencari
 def carijadwal(kota):
-    URLsolat = "https://time.siswadi.com/timezone/" + kota
+    URLteman = "https://time.siswadi.com/timezone/" + kota
     r = requests.get(URLteman)
     data = r.json()
     # print(data)
     status = data['time']['timezone']
     letak = data['location']['address']
     jam = data['time']['time']
-    ini="Lokasi : " + status + "\n" + "Kota anda: " + letak + "\n" + "Jam sholat : " + jam
+    # print("Daerah : " + status + "\n"+"kota : " + letak + "\n"+ "Jam sholat : " + jam)
+    a = "Daerah : " + status + "\n"+"kota : " + letak + "\n"+ "Jam sholat : " + jam
+    return a
 
-return (ini)
 
+# Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -64,23 +64,19 @@ def callback():
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-return 'OK'
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    text = event.message.text #simplify for receive message
+    text = event.message.text #simplify for receove message
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
-
-    data=text.split('-')
-    if(data[0]=='cek'):
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=carijadwal(data[1])))
-    else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Bukan menupakan kota di Indonesia pakai cek-(nama kota)"))
     
-   
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=carijadwal(text)))
+    
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=porta
+    app.run(host='0.0.0.0', port=port)
+
